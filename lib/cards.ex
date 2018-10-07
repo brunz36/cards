3b141efa552b8bd08c98fcaf4f14aa2254bd3b51
Bruno Ribeiro <brunz36@gmail.com>
2018-10-06 20:19:06 -0400

    Added load case with pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -25,10 +25,9 @@ defmodule Cards do
 	end
 
 	def load(filename) do
-		{status, binary} = File.read(filename)
-		case status do
-			:ok 		-> :erlang.binary_to_term(binary)
-			:error 	-> "Invalid File Name"
+		case File.read(filename) do
+			{:ok, binary} -> :erlang.binary_to_term(binary)
+			{:error, _reason}	-> "Invalid File Name"
 		end
 	end
 end
